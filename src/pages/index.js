09263d5cb4b17120a20d14b6fcd8527a10dcabb1
Sam Cowley <sfcowley@gmail.com>
2023-05-04 17:30:05 +0100

    CV PDF updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
                 A motivated and flexible front end developer with a drive to learn new skills and technologies. Driven by a desire to impact real world change through development. Explore my latest projects, showcasing my experience in a wide range of web technologies.
                 </p>
                 <div className='flex items-center self-start mt-2 lg:self-center'>
-                  <Link href="/sam-fearon_cv" target={"_blank"}
+                  <Link href="/sam-fearon_cv.pdf" target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6
                   rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
                   border-2 border-solid border-transparent hover:border-dark
@@ -49,7 +49,6 @@ export default function Home() {
                   "
                   download={true}
                   >CV <LinkArrow className={"w-6 ml-1"}/>
-                  
                   </Link>
                   <Link href="mailto:sfcowley@gmail.com" target={"_blank"}
                   className="ml-4 text-lg font-medium underline capitalize text-dark dark:text-light md:text-base"
