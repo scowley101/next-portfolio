cb20c34ecfbcb657f5fb2202d3e24d81957e891c
Sam Cowley <sfcowley@gmail.com>
2023-05-04 17:53:51 +0100

    z-index added to 'hire me module to fix bug

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -5,7 +5,7 @@ import { CircularText } from "./Icons";
 const HireMe = () => {
   return (
     <div
-      className="fixed flex items-center justify-center overflow-hidden left-4 bottom-4 md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute sm:right-0 "
+      className="fixed z-20 flex items-center justify-center overflow-hidden left-4 bottom-4 md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute sm:right-0"
     >
       <div className="relative flex items-center justify-center w-48 h-auto md:w-24">
         <CircularText
@@ -17,7 +17,7 @@ const HireMe = () => {
           className="flex items-center justify-center
 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark 
 text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full
-font-semibold hover:bg-light hover:text-dark
+font-semibold hover:bg-light hover:text-dark 
 
 dark:bg-primary dark:text-dark hover:dark:bg-dark hover:dark:text-light
                   hover:dark:border-light
